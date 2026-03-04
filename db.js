518b12cccbac84dfb11e0ce2e207c75afb20197b
hunghug538-debug <hunghug538@gmail.com>
2026-03-04 18:37:14 +0700

    upload sever code

--- db.js
+++ db.js
@@ -1,14 +1,11 @@
-const mysql = require('mysql2/promise');
-require('dotenv').config();
-
 const mysql = require("mysql2/promise");
 
-const connection = mysql.createPool({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT
 });
 
-module.exports = connection;
+module.exports = pool;
